e78c70fdf9006148d7dc9c79229794c05cea050c
Kai D. Gonzalez <gkai70264@gmail.com>
2021-12-10 17:06:03 -0500

    first commit

---
+++ src/error.d
@@ -0,0 +1,32 @@
+module error;
+
+// Copyright (C) 2021 Kai D. Gonzalez
+// 
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as
+// published by the Free Software Foundation, either version 3 of the
+// License, or (at your option) any later version.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Affero General Public License for more details.
+// 
+// You should have received a copy of the GNU Affero General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+import fmt;
+import std.stdio;
+
+void main(string[] args) {
+
+    version (DMD) 
+        writeln(formatn(stderr, "you are using D version {}, 
+        this hasn't been tested, and is most likely slower.", ["DMD"]));
+    version (LDC) 
+        writeln(formatn(stderr, "you are using D version {}, this hasn't been tested, and is most
+        likely slower.", ["LLVM"]));
+    if (args.length > 1) {    
+    writeln(formatn("\033[31;1merror: \033[0m{}", [args[1]]));
+    }
+}
